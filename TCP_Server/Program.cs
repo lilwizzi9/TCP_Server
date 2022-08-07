010d54816728d371b72d91761ac56cb54a1ebafc
lilwizzi9 <62917075+lilwizzi9@users.noreply.github.com>
2022-08-07 02:58:03 +0100

    Add project files.

---
+++ TCP_Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TCP_Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
